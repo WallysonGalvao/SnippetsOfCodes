b6294f3259eaa38e4f5b7e8159feafe16aef7bf5
Wallyson Galvão <wallyson.galvao@gmail.com>
2017-04-13 14:28:19 -0300

    Update DateConvert.js
    Alterado nome da variável sDateBraSplit para oDateBraSplit

--- DateConvert.js
+++ DateConvert.js
@@ -1,11 +1,11 @@
 // Declare and initialize the variable sDateBra as an String
 var sDateBra = "30/03/2017";
 
-// Declare and initialize the sDateBraSplit variable with the sDateBra variable split by the slash
-var sDateBraSplit = sDateBra.split("/");
+// Declare and initialize the oDateBraSplit variable with the sDateBra variable split by the slash
+var oDateBraSplit = sDateBra.split("/");
 
 // Declare and initialize the sDateUsa variable with sDateBraSplit formatted "03/30/2017"
-var sDateUsa = sDateBraSplit[1] + "/" + sDateBraSplit[0] + "/" + sDateBraSplit[2];
+var sDateUsa = oDateBraSplit[1] + "/" + oDateBraSplit[0] + "/" + oDateBraSplit[2];
 
 // Declare and initialize the oDate variable with a new Date passing as a parameter sDateUsa
 var oDate = new Date(sDateUsa);
