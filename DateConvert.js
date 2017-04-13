7b532efd47d1f230eec40fbcdf6ebada64ab7609
Wallyson Galvão <wallyson.galvao@gmail.com>
2017-04-13 14:09:11 -0300

    Add files via upload
    Code to format a variable of type String in a variable of type Date

---
+++ DateConvert.js
@@ -0,0 +1,4 @@
+var sDateBra = "30/03/2017";
+var sDateBraSplit = sDateBra.split("/");
+var sDateUsa = sDateBraSplit[1] + "/" + sDateBraSplit[0] + "/" + sDateBraSplit[2];
+var oDate = new Date(sDateUsa);
